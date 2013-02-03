fd82848473a4130740ad8b0c4941514583ea9f94
rossfadely <rossfadely@gmail.com>
2013-02-03 13:28:04 -0500

    refactor

--- _khedive.c
+++ _khedive.c
@@ -19,97 +19,83 @@ void KDE(long Nsamp, long Ndim,long *KDEtype, double *h, double *samples, double
     // Multivariate KDE, assumes input array (originally Nsamp x Ndim) is flattened.
     //
     
-    long i,j;
+    long i,j,k;
     
-    for (i=0; i<Nsamp; i++) {
+    for (k=0; k<Nsamp; k++) {
         pdf[i] = 0.0;
-        for (j=0; j<Ndim; j++) {
-            if (j==0) {
-                pdf[i] = KDE_1D(Nsamp,Ndim,i,j,KDEtype[j],h[j],samples);
-            } else {
-                pdf[i] *= KDE_1D(Nsamp,Ndim,i,j,KDEtype[j],h[j],samples);
-            }
-        }
-        pdf[i] *= 1.0 / (double)Nsamp;
+        
+        pdf[i] = 1.0 / 
     }
-    
     
 }
 
-double KDE_1D(long Nsamp,long Ndim,long i,long j,long KDEtype,double h, double *samples) {
+double kernel_estimate(long KDEtype, double h, double x_est, double x_exp) {
     //
-    // Do a 1D KDE
+    // Return the kernel estimate for the point being estimated (x_est),  
+    // using the given examplar (x_exp).
     //
     
-    long k, ind, idx;
-    double u, pdf=0.0;
+    double u, KE;
     const double pi = 3.14159265359;
     
-    ind = i * Ndim + j; // index of point being estimated
-    
-    for (k=0; k<Nsamp; k++) {
-        
-        // index of current sample along the jth dimension
-        idx = k * Ndim + j;
-        
-        // scaled difference
-        u = (samples[ind] - samples[idx]) / h;
-        
-        // begin kernel types
-        //
-        // Gaussian
-        if (KDEtype==0) {
-            if (ind!=idx) {
-                pdf += exp(-0.5 * u * u) / sqrt(2. * pi);
-            }
+                
+    // scaled difference
+    u = (x_est - x_exp) / h;
+
+    // begin kernel types
+    //
+    // Gaussian
+    if (KDEtype==0) {
+        if (ind!=idx) {
+            KE = exp(-0.5 * u * u) / sqrt(2. * pi) / h;   
         }
-        // Triangular
-        if (KDEtype==1) {
-            // why is abs() all screwy?
-            if (ind!=idx && u>-1.0 && u<1.0) {
-                pdf += 1.0 - sqrt(u * u);
-            }
+    }
+    // Triangular
+    if (KDEtype==1) {
+        // why is abs() all screwy?
+        if (ind!=idx && u>-1.0 && u<1.0) {
+            KE = 1.0 - sqrt(u * u) / h;
         }
-        // Epanechnikov
-        if (KDEtype==2) {
-            if (ind!=idx && u>-1.0 && u<1.0) {
-                pdf += 3. / 4. * (1.0 - u * u);
-            }
+    }
+    // Epanechnikov
+    if (KDEtype==2) {
+        if (ind!=idx && u>-1.0 && u<1.0) {
+            KE = 3. / 4. * (1.0 - u * u) / h;
         }
-        // Biweight
-        if (KDEtype==3) {
-            if (ind!=idx && u>-1.0 && u<1.0) {
-                pdf += 15. / 16. * pow((1.0 - u * u),2.0);
-            }
+    }
+    // Biweight
+    if (KDEtype==3) {
+        if (ind!=idx && u>-1.0 && u<1.0) {
+            KE = 15. / 16. * pow((1.0 - u * u),2.0) / h;
         }
-        // Triweight
-        if (KDEtype==4) {
-            if (ind!=idx && u>-1.0 && u<1.0) {
-                pdf += 35. / 32. * pow((1.0 - u * u),3.0);
-            }
+    }
+    // Triweight
+    if (KDEtype==4) {
+        if (ind!=idx && u>-1.0 && u<1.0) {
+            KE = 35. / 32. * pow((1.0 - u * u),3.0) / h;
         }
-        // Tricube
-        if (KDEtype==5) {
-            // why is abs() all screwy?
-            if (ind!=idx && u>-1.0 && u<1.0) {
-                pdf += 70. / 81. * pow((1.0 - sqrt(pow(u,6.0))),3.0);
-            }
+    }
+    // Tricube
+    if (KDEtype==5) {
+        // why is abs() all screwy?
+        if (ind!=idx && u>-1.0 && u<1.0) {
+            KE = 70. / 81. * pow((1.0 - sqrt(pow(u,6.0))),3.0) / h;
         }
-        // Cosine
-        if (KDEtype==6) {
-            if (ind!=idx && u>-1.0 && u<1.0) {
-                pdf += 0.25 * pi * cos(0.5 * pi * u);
-            }
+    }
+    // Cosine
+    if (KDEtype==6) {
+        if (ind!=idx && u>-1.0 && u<1.0) {
+            KE = 0.25 * pi * cos(0.5 * pi * u) / h;
         }
-        // Uniform
-        if (KDEtype==7) {
-            if (ind!=idx && u>-1.0 && u<1.0) {
-                pdf += 0.5;
-            }
+    }
+    // Uniform
+    if (KDEtype==7) {
+        if (ind!=idx && u>-1.0 && u<1.0) {
+            KE = 0.5 / h;
         }
     }
     
-    pdf *= 1.0 / h;
-    
-    return pdf;
+    return KE;
 }
+    
+    
